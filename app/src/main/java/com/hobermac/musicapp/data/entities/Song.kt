700f837369a7ee8ce9ccce092497637a675fdc3a
Anton1636 <hobermac228@gmil.com>
2023-01-24 19:28:47 +0200

    Create Project
    Add Firebase connection

---
+++ app/src/main/java/com/hobermac/musicapp/data/entities/Song.kt
@@ -0,0 +1,9 @@
+package com.hobermac.musicapp.data.entities
+
+data class Song (
+    val mediaId: String = "",
+    val title: String = "",
+    val subtitle: String = "",
+    val songURL: String = "",
+    val imageURL: String = ""
+)
---
+++ app/src/main/java/com/hobermac/musicapp/other/Constants.kt
@@ -0,0 +1,5 @@
+package com.hobermac.musicapp.other
+
+object Constants {
+    const val SONG_COLLECTION = "songs"
+}
